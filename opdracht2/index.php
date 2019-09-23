e04b661d7539909ee49b45add335873dec0dad1b
RickBodewes <Rick@planet.nl>
2019-09-23 09:23:37 +0200

    opdracht2

---
+++ opdracht2/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Opdracht1</title>
+</head>
+    <body>
+        <?php
+            $boodschap = "aardappel";
+            echo "ik moet vandaag ".$boodschap."en kopen.";
+        ?>
+    </body>
+</html>
